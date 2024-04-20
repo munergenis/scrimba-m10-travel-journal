13035f612cb220731646a881bbd451209636058a
munergenis <154993411+munergenis@users.noreply.github.com>
2024-04-20 23:58:20 +0200

    added remaining data

--- src/data/data.js
+++ src/data/data.js
@@ -12,41 +12,45 @@ export default [
   {
     title: "Grand Canyon",
     location: "Arizona, USA",
-    googleMapsUrl: "<BUSCAR DIRECCION>",
+    googleMapsUrl: "https://maps.app.goo.gl/mJh96b1jGXtXmbwZ8",
     startDate: "15 Apr, 2021",
     endDate: "20 Apr, 2021",
     description:
       "The Grand Canyon is one of the most famous natural attractions in the world, known for its stunning beauty and breathtaking views. Carved by the Colorado River over millions of years, it offers numerous hiking trails and overlooks for visitors to explore.",
-    imageUrl: "<BUSCAR IMAGEN>",
+    imageUrl:
+      "https://images.unsplash.com/photo-1501258338179-b25f87809429?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     title: "Eiffel Tower",
     location: "Paris, France",
-    googleMapsUrl: "<BUSCAR DIRECCION>",
+    googleMapsUrl: "https://maps.app.goo.gl/H4eg6mgb64TmXMxBA",
     startDate: "5 Jun, 2021",
     endDate: "10 Jun, 2021",
     description:
       "The Eiffel Tower is one of the most iconic landmarks in the world, symbolizing the romance and beauty of Paris. Standing at 324 meters (1,063 feet) tall, it offers breathtaking views of the city from its observation decks.",
-    imageUrl: "<BUSCAR IMAGEN>",
+    imageUrl:
+      "https://images.unsplash.com/photo-1568668392383-58c369615742?q=80&w=1919&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     title: "Machu Picchu",
     location: "Cusco Region, Peru",
-    googleMapsUrl: "<BUSCAR DIRECCION>",
+    googleMapsUrl: "https://maps.app.goo.gl/9jb3rSPn67xS4mdC7",
     startDate: "20 Sep, 2021",
     endDate: "28 Sep, 2021",
     description:
       "Machu Picchu is an ancient Incan citadel set high in the Andes Mountains of Peru. Built in the 15th century and later abandoned, it's renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views.",
-    imageUrl: "<BUSCAR IMAGEN>",
+    imageUrl:
+      "https://images.unsplash.com/photo-1509216242873-7786f446f465?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     title: "Sydney Opera House",
     location: "Sydney, Australia",
-    googleMapsUrl: "<BUSCAR DIRECCION>",
+    googleMapsUrl: "https://maps.app.goo.gl/UhLz8rKuX2LjXTba6",
     startDate: "10 Dec, 2021",
     endDate: "15 Dec, 2021",
     description:
       "The Sydney Opera House is one of the most distinctive and famous buildings of the 20th century. It is located on Bennelong Point in Sydney Harbour, close to the Sydney Harbour Bridge. It is an iconic symbol of Australia, known for its unique sail-like design.",
-    imageUrl: "<BUSCAR IMAGEN>",
+    imageUrl:
+      "https://images.unsplash.com/photo-1598502484949-4348a68fb312?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ]
