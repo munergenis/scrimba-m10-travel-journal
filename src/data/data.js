51538e9004afb91e49b6f9bd033bc339c4d17523
munergenis <154993411+munergenis@users.noreply.github.com>
2024-04-20 22:17:02 +0200

    Initial commit

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    title: "Grand Canyon",
+    location: "Arizona, USA",
+    googleMapsUrl: "<BUSCAR DIRECCION>",
+    startDate: "15 Apr, 2021",
+    endDate: "20 Apr, 2021",
+    description:
+      "The Grand Canyon is one of the most famous natural attractions in the world, known for its stunning beauty and breathtaking views. Carved by the Colorado River over millions of years, it offers numerous hiking trails and overlooks for visitors to explore.",
+    imageUrl: "<BUSCAR IMAGEN>",
+  },
+  {
+    title: "Eiffel Tower",
+    location: "Paris, France",
+    googleMapsUrl: "<BUSCAR DIRECCION>",
+    startDate: "5 Jun, 2021",
+    endDate: "10 Jun, 2021",
+    description:
+      "The Eiffel Tower is one of the most iconic landmarks in the world, symbolizing the romance and beauty of Paris. Standing at 324 meters (1,063 feet) tall, it offers breathtaking views of the city from its observation decks.",
+    imageUrl: "<BUSCAR IMAGEN>",
+  },
+  {
+    title: "Machu Picchu",
+    location: "Cusco Region, Peru",
+    googleMapsUrl: "<BUSCAR DIRECCION>",
+    startDate: "20 Sep, 2021",
+    endDate: "28 Sep, 2021",
+    description:
+      "Machu Picchu is an ancient Incan citadel set high in the Andes Mountains of Peru. Built in the 15th century and later abandoned, it's renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments, and panoramic views.",
+    imageUrl: "<BUSCAR IMAGEN>",
+  },
+  {
+    title: "Sydney Opera House",
+    location: "Sydney, Australia",
+    googleMapsUrl: "<BUSCAR DIRECCION>",
+    startDate: "10 Dec, 2021",
+    endDate: "15 Dec, 2021",
+    description:
+      "The Sydney Opera House is one of the most distinctive and famous buildings of the 20th century. It is located on Bennelong Point in Sydney Harbour, close to the Sydney Harbour Bridge. It is an iconic symbol of Australia, known for its unique sail-like design.",
+    imageUrl: "<BUSCAR IMAGEN>",
+  },
+]
